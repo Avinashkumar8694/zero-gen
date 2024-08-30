c43c0bdd509324ad912691850da5bb4b150ef43d
Avinash Gupta <avinash.gupta@neutrinos.co>
2024-08-30 14:33:51 +0530

    fix: updated version

--- src/utill/fsUtill.js
+++ src/utill/fsUtill.js
@@ -46,7 +46,7 @@ export const addDefaultDependencies = pkgJson => {
     pkgJson.devDependencies = {
         lit: '^3.1.4',
         typescript: '^5.2.2',
-        'zero-annotation': '^1.0.9',
+        'zero-annotation': '^1.0.10',
     };
     pkgJson.dependencies = {
         express: '^4.19.2',
